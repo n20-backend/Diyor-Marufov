f28c1c8088e81a8afd5c564c9d804d75e1a00b58
DiyorMarufov <marufovdiyor9@gmail.com>
2025-04-07 10:59:38 +0500

    Validation done

--- src/config/env.js
+++ src/config/env.js
@@ -5,9 +5,5 @@ const { Pool } = pg;
 dotenv.config();
 
 export const connection = new Pool({
-  user: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASSWORD || "2703",
-  host: process.env.DB_HOST || "localhost",
-  database: process.env.DB_DATABASE || "e_commerce_project",
-  port: process.env.DB_PORT || 5432,
+  connectionString: process.env.DB_CONNECTION
 });
